dd3922c3683ff93e09fa34b4834347bb1b760301
iwason <j.iwamura1986@gmail.com>
2017-10-29 16:01:09 +0900

    Rails tutorial 8章 完了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,12 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  # 現在のユーザーをログアウトする
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
