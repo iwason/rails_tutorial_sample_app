35193c41133439733d312b68ad611bbcb9278c94
iwason <j.iwamura1986@gmail.com>
2017-10-31 08:38:55 +0900

    Rails tutorial 9章 途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,13 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
 end
